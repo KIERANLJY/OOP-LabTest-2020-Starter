ebe59e027d99010220d3ed519943963d48e279fc
Your Name <you@example.com>
2020-04-22 11:35:54 +0100

    loaded csv file and printed

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -1,9 +1,15 @@
 package ie.tudublin;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
 
 public class Gantt extends PApplet
-{	
+{
+
+	public ArrayList<Task> tasks = new ArrayList<Task>();
 	
 	public void settings()
 	{
@@ -12,12 +18,20 @@ public class Gantt extends PApplet
 
 	public void loadTasks()
 	{
-		
+		Table t = loadTable("tasks.csv", "header");
+		for(TableRow row:t.rows())
+		{
+			Task task = new Task(row);
+			tasks.add(task);
+		}
 	}
 
 	public void printTasks()
 	{
-		
+		for(Task t:tasks)
+		{
+			println(t.getTask() + ", " + t.getStart() + ", " + t.getEnd());
+		}
 	}
 	
 	public void mousePressed()
@@ -34,6 +48,8 @@ public class Gantt extends PApplet
 	
 	public void setup() 
 	{
+		loadTasks();
+		printTasks();
 	}
 	
 	public void draw()
---
+++ java/src/ie/tudublin/Task.java
@@ -0,0 +1,40 @@
+package ie.tudublin;
+
+import processing.data.TableRow;
+
+public class Task 
+{
+    
+    private String task;
+    private int start;
+    private int end;
+
+    public Task(String task, int start, int end)
+    {
+        this.task = task;
+        this.start = start;
+        this.end = end;
+    }
+
+    public Task(TableRow tr)
+    {
+        this(tr.getString("Task"), tr.getInt("Start"), tr.getInt("End"));
+    }
+
+    public String toString()
+    {
+        return this.task + ", " + this.start + ", " + this.end;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+}
